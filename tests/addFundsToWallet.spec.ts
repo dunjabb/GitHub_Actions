2065a240b67622bf00867b4deeeca875651f40d0
dunjabb <106536154+dunjabb@users.noreply.github.com>
2024-04-08 15:45:33 +0200

    feat: add function for messages [GH-1] (#14)
    * feat: add function for messages [GH-1]
    
    * feat: write test results file [GH-1]
    
    * feat: add date, fix typo [GH-1]
    
    ---------
    
    Co-authored-by: dunjabb <dunja.boskovic@bloxico.com>

--- tests/addFundsToWallet.spec.ts
+++ tests/addFundsToWallet.spec.ts
@@ -1,14 +1,16 @@
 import { test } from "@playwright/test";
+import fs from "fs";
 const walletAddresses = JSON.parse(
   JSON.stringify(require("../tests/wallet-address.json"))
 );
 
 test("Add funds to wallet address", async ({ page }) => {
-  await page.goto('https://faucet.flare.network/');
-  await page
-    .locator('//div[@class="m-8bffd616 mantine-Flex-root __m__-Rardmullfdla"]')
-    .click();
+  await page.goto("https://faucet.flare.network/");
+  await page.locator('(//div[contains(@class, "m-8bffd616")])[1]').click();
   await page.locator('//p[text()="Coston2"]').click();
+  let successfulTransactions = 0;
+  let failedTransactions = 0;
+  let results: any[] = [];
 
   for (const address of walletAddresses) {
     await page.waitForTimeout(2000);
@@ -16,5 +18,26 @@ test("Add funds to wallet address", async ({ page }) => {
     await page.waitForTimeout(2000);
     await page.locator('//button[@type="submit"]').click();
     await page.waitForTimeout(2000);
+
+    const messagePlaceholder = await page.locator('(//p[contains(@class, "Text-root")])[5]');
+    const message = await messagePlaceholder.innerText();
+    results.push( message);
+
+    if (message.includes("Sent")) {
+      console.log(`${message}`);
+      successfulTransactions++; 
+    } else if (message.includes("wait up") || message.includes("captcha")) {
+      console.log(`${message}`);
+      failedTransactions++;
+    }
   }
+
+  results.push(`Successful transfer: ${successfulTransactions} \nFailed transfer: ${failedTransactions}`);
+  const date = new Date().toDateString();
+  const fileData = `Date: ${date}\n${results.join("\n")}`;
+    fs.writeFile("results.txt", fileData, (error) => {
+    if (error) {
+      console.error("An error occurred while writing to the file:", error);
+    }
+  });
 });
